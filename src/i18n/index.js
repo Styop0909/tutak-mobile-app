5ab3efbaf309ed08f3b49ab3e7304e7f7128c7b9
Styop0909 <stepan.grig.2009@gmail.com>
2026-07-07 17:33:01 +0400

    Update index.js

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,2 +1,25 @@
-// i18n  
-export const i18n = {};  
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+import hy from './locales/hy.json';
+import ru from './locales/ru.json';
+import en from './locales/en.json';
+
+i18n.translations = { hy, ru, en };
+i18n.locale = Localization.locale;
+i18n.fallbacks = true;
+i18n.defaultLocale = 'en';
+
+export const initI18n = () => {
+  const locale = Localization.locale;
+  i18n.locale = locale;
+};
+
+export const t = (key, params = {}) => {
+  return i18n.t(key, params);
+};
+
+export const changeLanguage = (lang) => {
+  i18n.locale = lang;
+};
+
+export default i18n;
