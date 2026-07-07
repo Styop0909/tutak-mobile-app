965b66e41f9bbd1b5fa3d60d6326621352e2792b
Styop0909 <stepan.grig.2009@gmail.com>
2026-07-07 17:12:20 +0400

    Update menu.js

--- src/api/menu.js
+++ src/api/menu.js
@@ -1,2 +1,18 @@
-// Menu API  
-export const menuApi = { getMenu: () = };  
+import { api } from './client';
+
+export const menuApi = {
+  getMenu: async (city) => {
+    const response = await api.get('/api/menu', { city });
+    return response.data;
+  },
+
+  getCategories: async () => {
+    const response = await api.get('/api/menu/categories');
+    return response.data;
+  },
+
+  getMenuItem: async (id) => {
+    const response = await api.get(`/api/menu/${id}`);
+    return response.data;
+  },
+};
