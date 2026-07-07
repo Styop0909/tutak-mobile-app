6a26d82362437bef8eba53974a722ca655c6b232
Styop0909 <stepan.grig.2009@gmail.com>
2026-07-07 17:27:29 +0400

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,3 +1,20 @@
-// App Navigator  
-import React from 'react';  
-export const AppNavigator = () = 
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { useAuthStore } from '../store/authStore';
+import { AuthNavigator } from './AuthNavigator';
+import { MainTabNavigator } from './MainTabNavigator';
+import { Loading } from '../components/common/Loading';
+
+export const AppNavigator = () => {
+  const { isAuthenticated, isLoading } = useAuthStore();
+
+  if (isLoading) {
+    return <Loading fullScreen />;
+  }
+
+  return (
+    <NavigationContainer>
+      {isAuthenticated ? <MainTabNavigator /> : <AuthNavigator />}
+    </NavigationContainer>
+  );
+};
