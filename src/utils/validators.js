548119d41eff7f7693a37e21d98521cc7ef3446b
Styop0909 <stepan.grig.2009@gmail.com>
2026-07-07 17:32:37 +0400

    Create validators.js

---
+++ src/utils/validators.js
@@ -0,0 +1,21 @@
+export const validatePhone = (phone) => {
+  const phoneRegex = /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/;
+  return phoneRegex.test(phone);
+};
+
+export const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+export const validateRequired = (value) => {
+  return value && value.trim().length > 0;
+};
+
+export const validateMinLength = (value, minLength) => {
+  return value && value.length >= minLength;
+};
+
+export const validateMaxLength = (value, maxLength) => {
+  return value && value.length <= maxLength;
+};
