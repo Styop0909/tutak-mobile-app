49ffa0a9a74d019ad582218790e4b2fc27a93391
Styop0909 <stepan.grig.2009@gmail.com>
2026-07-07 17:12:10 +0400

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,2 +1,28 @@
-// Auth API  
-export const authApi = { login: () = };  
+import { api } from './client';
+
+export const authApi = {
+  requestOTP: async (phoneNumber) => {
+    const response = await api.post('/api/auth/request-otp', { phoneNumber });
+    return response.data;
+  },
+
+  verifyOTP: async (phoneNumber, code) => {
+    const response = await api.post('/api/auth/verify-otp', { phoneNumber, code });
+    return response.data;
+  },
+
+  getCurrentUser: async () => {
+    const response = await api.get('/api/user');
+    return response.data;
+  },
+
+  loginWithTelegram: async (telegramData) => {
+    const response = await api.post('/api/auth/telegram', telegramData);
+    return response.data;
+  },
+
+  logout: async () => {
+    const response = await api.post('/api/auth/logout');
+    return response.data;
+  },
+};
