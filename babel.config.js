3fb1e2f22ca032d10afe6d033774a69ab6d4146e
Styop0909 <stepan.grig.2009@gmail.com>
2026-07-07 17:35:35 +0400

    Update babel.config.js to include presets and plugins

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,17 @@
-module.exports = {} 
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@': './src',
+          },
+        },
+      ],
+    ],
+  };
+};
